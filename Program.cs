42f2f078150f0a3ea89d491699404c12130ca98b
Andre McElroy jr <amcelroy50744@gmail.com>
2021-09-13 13:30:13 -0500

    if else statements

--- Program.cs
+++ Program.cs
@@ -44,6 +44,30 @@ namespace PracticeSeptember
             userAgeList.Insert(2, 34);
             WriteLine(myCount);
 
+            int userAge;
+            Write("Please enter your age:");
+            userAge = Convert.ToInt32(ReadLine());
+
+            if (userAge < 0 || userAge > 100)
+            {
+                WriteLine("Invalid Age");
+                WriteLine("Age must be between 0 and 100");
+            }
+            else if  (userAge < 18) 
+            {
+                WriteLine("Sorry you are underage");
+
+            }
+            else if (userAge < 21) 
+            {
+                 WriteLine("You need parental consent");
+            }
+            else 
+            {
+                WriteLine("Congratulations!");
+                WriteLine("You may sign up for the event!");
+            }
+
 
         }
     }
