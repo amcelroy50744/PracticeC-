ca8a64a6346ec63d2376b8f23fc34a25ab6e6084
Andre McElroy jr <amcelroy50744@gmail.com>
2021-09-13 13:57:08 -0500

    for ex

--- Program.cs
+++ Program.cs
@@ -68,6 +68,38 @@ namespace PracticeSeptember
                 WriteLine("You may sign up for the event!");
             }
 
+            Write("Enter your grade: ");
+            string userGrade = ReadLine();
+
+            switch (userGrade) 
+            {
+                case "A+":
+                case "A":
+                    Console.WriteLine("Distinction");
+                    break;
+                case "B+":
+                case "B":
+                    Console.WriteLine("B Grade");
+                    break;
+                case "C":
+                    Console.WriteLine("C Grade");
+                    break;
+                default:
+                    Console.WriteLine("Fail");
+                    break;
+
+            }
+
+            for (int i = 0; i <= 5; i++)
+            {
+                WriteLine(i);
+            }
+
+            int[] myNumbers = { 10, 20, 30, 40, 50 };
+            for (int i = 0; i < myNumbers.Length; i++)
+            {
+                WriteLine(myNumbers[i]);
+            }
 
         }
     }
